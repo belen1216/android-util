6b44b980e7d9ba8283080c54f85664808f186362
belen1216 <belen1216@protonmail.com>
2019-05-10 16:45:34 +0800

    Initial commit

---
+++ sample/src/main/java/com/belen1216/android/util/sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.belen1216.android.util.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
