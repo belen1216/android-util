8c07151c2e0d1619fd2d827b32a5b464da27aec4
belen1216 <belen1216@protonmail.com>
2019-05-10 18:38:37 +0800

    test

--- sample/src/main/java/com/belen1216/android/util/sample/MainActivity.java
+++ sample/src/main/java/com/belen1216/android/util/sample/MainActivity.java
@@ -2,6 +2,9 @@ package com.belen1216.android.util.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.belen1216.android.util.NetWorkUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String typeName = NetWorkUtils.getNetworkTypeName(this);
+        Log.d("TAG", typeName);
     }
 }
